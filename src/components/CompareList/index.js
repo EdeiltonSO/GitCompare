ebc8c31e1f88ceb6cc3b729f8b048d97dacdfac4
EdeiltonSO <edeiltonso@gmail.com>
2018-11-29 18:09:14 -0200

    Validando propriedades

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container, Repository } from './styles';
 
 const CompareList = ({ repositories }) => (
   <Container>
     {repositories.map(repository => (
-      <Repository>
+      <Repository id={repository.id}>
         <header>
           <img src={repository.owner.avatar_url} alt="Facebook" />
           <strong>{repository.name}</strong>
@@ -30,5 +31,24 @@ const CompareList = ({ repositories }) => (
     ))}
   </Container>
 );
+
+// propTypes com inicial minúscula é
+// palavra reservada do React pra componentes
+CompareList.propTypes = {
+  repositories: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      owner: PropTypes.shape({
+        login: PropTypes.string,
+        avatar_url: PropTypes.string,
+      }),
+      stargazers_count: PropTypes.number,
+      forks: PropTypes.number,
+      open_issues_count: PropTypes.number,
+      pushed_at: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default CompareList;
